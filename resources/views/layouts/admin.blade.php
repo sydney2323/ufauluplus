bd8695a0236ac3900fcdd8a9e6e46ed370b08a2b
sydney2323 <sydneykb38@gmail.com>
2022-09-17 06:40:46 +0300

    feat: Add secure_asset method on all href

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,21 +10,21 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('plugins/fontawesome-free/css/all.min.css') }}">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="{{ asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
   <!-- iCheck -->
-  <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('dist/css/adminlte.min.css') }}">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="{{ asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('plugins/daterangepicker/daterangepicker.css') }}">
   <!-- summernote -->
-  <link rel="stylesheet" href="{{ asset('plugins/summernote/summernote-bs4.min.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('plugins/summernote/summernote-bs4.min.css') }}">
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
@@ -238,36 +238,36 @@
 
 
 <!-- jQuery -->
-<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ secure_asset('plugins/jquery/jquery.min.js') }}"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+<script src="{{ secure_asset('plugins/jquery-ui/jquery-ui.min.js') }}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ secure_asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- ChartJS -->
-<script src="{{ asset('plugins/chart.js/Chart.min.js') }}"></script>
+<script src="{{ secure_asset('plugins/chart.js/Chart.min.js') }}"></script>
 <!-- jQuery Knob Chart -->
-<script src="{{ asset('plugins/jquery-knob/jquery.knob.min.js') }}"></script>
+<script src="{{ secure_asset('plugins/jquery-knob/jquery.knob.min.js') }}"></script>
 <!-- daterangepicker -->
-<script src="{{ asset('plugins/moment/moment.min.js') }}"></script>
-<script src="{{ asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
+<script src="{{ secure_asset('plugins/moment/moment.min.js') }}"></script>
+<script src="{{ secure_asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="{{ asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
+<script src="{{ secure_asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
 <!-- Summernote -->
-<script src="{{ asset('plugins/summernote/summernote-bs4.min.js') }}"></script>
+<script src="{{ secure_asset('plugins/summernote/summernote-bs4.min.js') }}"></script>
 <!-- jquery-validation -->
-<script src="{{ asset('plugins/jquery-validation/jquery.validate.min.js') }}"></script>
-<script src="{{ asset('plugins/jquery-validation/additional-methods.min.js') }}"></script>
+<script src="{{ secure_asset('plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+<script src="{{ secure_asset('plugins/jquery-validation/additional-methods.min.js') }}"></script>
 <!-- SweetAlert2 -->
-<script src="{{ asset('plugins/sweetalert2/sweetalert2.min.js') }}"></script>
-<script src="{{ asset('plugins/toastr/toastr.min.js') }}"></script>
+<script src="{{ secure_asset('plugins/sweetalert2/sweetalert2.min.js') }}"></script>
+<script src="{{ secure_asset('plugins/toastr/toastr.min.js') }}"></script>
 <!-- overlayScrollbars -->
-<script src="{{ asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+<script src="{{ secure_asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset('dist/js/adminlte.js') }}"></script>
+<script src="{{ secure_asset('dist/js/adminlte.js') }}"></script>
 <script>
     $('#summernote').summernote({
         placeholder:'Enter description..',
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,14 +12,14 @@
     <title>UfauluPlus+</title>
 
      <!-- Bootstrap core CSS -->
-     <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+     <link href="{{ secure_asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
 
      <!-- Additional CSS Files -->
-     <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}">
-     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-     <link rel="stylesheet" href="{{ asset('assets/css/owl.css') }} ">
-     <link rel="stylesheet" href="{{ asset('assets/css/lightbox.css') }}">
+     <link rel="stylesheet" href="{{ secure_asset('assets/css/fontawesome.css') }}">
+     <link rel="stylesheet" href="{{ secure_asset('assets/css/style.css') }}">
+     <link rel="stylesheet" href="{{ secure_asset('assets/css/owl.css') }} ">
+     <link rel="stylesheet" href="{{ secure_asset('assets/css/lightbox.css') }}">
 
     <style>
       .img {
@@ -129,16 +129,16 @@ https://templatemo.com/tm-569-edu-meeting
 
   <!-- Scripts -->
   <!-- Bootstrap core JavaScript -->
-  <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-  <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ secure_asset('vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ secure_asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
-  <script src="{{ asset('assets/js/isotope.min.js') }}"></script>
-  <script src="{{ asset('assets/js/owl-carousel.js') }}"></script>
-  <script src="{{ asset('assets/js/lightbox.js') }}"></script>
-  <script src="{{ asset('assets/js/tabs.js') }}"></script>
-  <script src="{{ asset('assets/js/video.js') }}"></script>
-  <script src="{{ asset('assets/js/slick-slider.js') }}"></script>
-  <script src="{{ asset('assets/js/custom.js') }}"></script>
+  <script src="{{ secure_asset('assets/js/isotope.min.js') }}"></script>
+  <script src="{{ secure_asset('assets/js/owl-carousel.js') }}"></script>
+  <script src="{{ secure_asset('assets/js/lightbox.js') }}"></script>
+  <script src="{{ secure_asset('assets/js/tabs.js') }}"></script>
+  <script src="{{ secure_asset('assets/js/video.js') }}"></script>
+  <script src="{{ secure_asset('assets/js/slick-slider.js') }}"></script>
+  <script src="{{ secure_asset('assets/js/custom.js') }}"></script>
 </body>
 
 </body>
